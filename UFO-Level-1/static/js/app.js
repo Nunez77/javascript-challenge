77dd7704b6df56ea412efdda87190053f65389df
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-06-26 12:06:29 -0500

    Level 1 apparently done

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -18,8 +18,7 @@ var button = d3.select("#filter-btn");
 var inputFieldDate = d3.select("#datetime");
 // Body
 var $tbody = d3.select("tbody");
-// City
-var inputFieldCity = d3.select("#city");
+
 // Columns
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
@@ -34,18 +33,14 @@ var addData = (dataInput) => {
 
 addData(tableData);
 
-/ Creating an Event Listener for the Button
-// Setting up the Filter Button for Date and City
+// Setting up the Filter Button for Date
 button.on("click", () => {
 
     d3.event.preventDefault();
-    
 
     var inputDate = inputFieldDate.property("value").trim();
-    
 
     var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
-    
 
     $tbody.html("");
 
@@ -53,14 +48,10 @@ button.on("click", () => {
         filterDate
     }
 
-
     if(response.filterDate.length !== 0) {
         addData(filterDate);
     }
-
-    // Top if only works for filtering the date
-    
         else {
-            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+            $tbody.append("tr").append("td").text("No sightings reported at the given date.");
         }
 })
