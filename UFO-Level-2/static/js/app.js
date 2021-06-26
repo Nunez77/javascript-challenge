a1483352ed3e33ddc75aa2e9544d764464a87ea2
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-06-26 18:35:32 -0500

    giving up

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -11,86 +11,82 @@
 // 4. country
 // 5. shape
 
-// from data.js
+// Bring data from file
 var tableData = data;
 
 // YOUR CODE HERE!
 
-// Create references
-// Body
-var $tbody = d3.select("tbody");
-// Button
-var button = d3.select("#filter-btn");
-// Form
-var form = d3.select("#form");
-// Original list
-var filters = d3.select("#filters");
-// Available variables for users
-var inputFieldDate = d3.select("#datetime");
-var inputFieldCity = d3.select("#city");
-var inputFieldState = d3.select("#state");
-var inputFieldCountry = d3.select("#country");
-var inputFieldShape = d3.select("#shape");
+// Table refereces
+var tbody = d3.select("tbody");
 
-// Columns
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+// Function to build new table according to selected criteria
+function buildTable(tableData) {
+  // Clear previous search data
+  tbody.html("");
 
-// Create function
-function criteriaTable(table, data) {
-    tableHtml= d3.select("tbody")
-    tableHtml.html("")
-    // For each column in the data set
-    for (let element of data) {
-      // Insert row each time criteria is met
-      let row = table.insertRow();
-      // For each row in each column
-      for (key in element) {
-        // Insert new cell in each column each time criteria is met
-        let cell = row.insertCell();
-        // Creates text node. A text node encapsulates XML character content.
-        let text = document.createTextNode(element[key]);
-        // Appends row in column that met criteria to the new text node
-        cell.appendChild(text);
+  // For each row that meets criteria, insert new row
+  tableData.forEach((dataRow) => {
+
+    // Append new row to filtered table
+    var row = tbody.append("tr");
+
+    // For each row that meets the criteria insert append each column
+    Object.values(dataRow).forEach((val) => {
+      var cell = row.append("td");
+        cell.text(val);
       }
-    }
-  };
-  // Assign original table to variable
-  let table = document.querySelector("tbody");
-  criteriaTable(table, tableData);
- // --------------------------------------------
-
- // Apply user selections
-function filterData(userInput){
-    input=d3.select(userInput);
-    inputValue=input.property("value");  
-  return inputValue
+    );
+  });
 }
 
-// select the button(event)
-filterButton=d3.select("#filter-btn");
+// Identify user input and use the selected criteria to apply a filter to the data
 
-// call `on` (event lessener) to run the function that will work 
-filterButton.on("click",() => {
-   // filter the input from the table
-    filterTable=tableData.filter(item => (item.datetime === filterData("#datetime") || filterData("#datetime") === "") 
-    && (item.city === filterData("#city") || filterData("#city") === "")
-    && (item.state === filterData("#state") || filterData("#state") === "")
-    && (item.country === filterData("#country") || filterData("#country") === "") 
-    && (item.shape === filterData("#shape") || filterData("#shape") === ""));
-    console.log("filterTable",filterTable);
-    criteriaTable(table,filterTable);
+function applycriteria() {
+
+  // Prevent browser from executing default action 
+  d3.event.preventDefault();
+
+  // Get user selections to stablish search/filter criteria
+  var date = d3.select("#datetime").property("value");
+  var city = d3.select("#city").property("value");
+  var state = d3.select("#state").property("value");
+  var country = d3.select("#country").property("value");
+  var shape = d3.select("#shape").property("value");
+
+  let filteredData = tableData;
+
+  // If user wants a specific date, apply it to the filter
+  if (date) {
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+
+  // If user wants specific city, apply it to the filter
+  if (city) {
+    filteredData = filteredData.filter(row => row.city === city);
+  }
+
+  // If specific state is selected, apply it to the filter
+  if (state) {
+    filteredData = filteredData.filter(row => row.state === state);
+  }
+
+  // If specific country is selected, apply ti to the filter
+  if (country) {
+    filteredData = filteredData.filter(row => row.country === country);
+  }
+
+  // If specific shape is selected, apply to filter
+  if (shape) {
+    filteredData = filteredData.filter(row => row.shape === shape);
+  }
+
+ // Build final table
+  buildTable(filteredData);
+}
+
+// When button is clicked, display table according to selections
+d3.selectAll("#filter-btn").on("click", applycriteria);
 
 
-});
-
-// reset the table from the begnning button 
-resetButton=d3.select("#reset-btn")
-resetButton.on("click",() => {
-    criteriaTable(table,tableData)
-    // clear the input fileds 
-    document.getElementById('datetime').value = ''
-    document.getElementById('city').value = ''
-    document.getElementById('state').value = ''
-    document.getElementById('country').value = ''
-    document.getElementById('shape').value = ''
-});
+// Pending
+buildTable(tableData);
