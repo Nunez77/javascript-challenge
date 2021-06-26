7442756cd101931d748daadf2200ca0c48e4a70c
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-06-26 11:21:37 -0500

    Created references

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,24 +1,25 @@
-// Complete all of Level 1 criteria.
-// Using multiple input tags and/or select dropdowns, 
-// write JavaScript code so the user can to set multiple filters and search for UFO sightings
-// using the following criteria based on the table columns:
-// 1. date/time
-// 2. city
-// 3. state
-// 4. country
-// 5. shape
+// Create a basic HTML web page or use the index.html file provided (we recommend building your own custom page!).
+// Using the UFO dataset provided in the form of an array of JavaScript objects,
+// write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+// Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
+// Use a date form in your HTML document and write JavaScript code that will listen
+// for events and search through the date/time column to find rows that match user input.
 
 
 // from data.js
 var tableData = data;
+
+// YOUR CODE HERE!
 
 // Create references
 // Button
 var button = d3.select("#filter-btn");
 // Form
 var inputFieldDate = d3.select("#datetime");
-// Other
+// Body
 var $tbody = d3.select("tbody");
+// City
 var inputFieldCity = d3.select("#city");
+// Columns
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-// YOUR CODE HERE!
