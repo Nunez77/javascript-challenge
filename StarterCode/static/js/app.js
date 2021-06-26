4f555280382da05e675f2609ad11127b3a57e1c9
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-06-26 11:12:44 -0500

    Back to level 1

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,14 @@
+// Complete all of Level 1 criteria.
+// Using multiple input tags and/or select dropdowns, 
+// write JavaScript code so the user can to set multiple filters and search for UFO sightings
+// using the following criteria based on the table columns:
+// 1. date/time
+// 2. city
+// 3. state
+// 4. country
+// 5. shape
+
+
 // from data.js
 var tableData = data;
 
