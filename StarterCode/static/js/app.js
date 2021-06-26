1b85831c4539c28cc82b7f43a9a8a10f042cc507
Fernando Nunez <79233160+Nunez77@users.noreply.github.com>
2021-06-26 11:06:18 -0500

    began general app

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
+// Create references
+// Button
+var button = d3.select("#filter-btn");
+// Form
+var inputFieldDate = d3.select("#datetime");
+// Other
+var $tbody = d3.select("tbody");
+var inputFieldCity = d3.select("#city");
+
 // YOUR CODE HERE!
